5854c434d68cec97362c095684eeeae97ec4e099
Anonymous User <golobal@gmail.com>
2025-06-20 04:42:25 +0100

    Refactor authentication routes: Move guest routes to web.php and update login view

--- routes/users.php
+++ routes/users.php
@@ -11,19 +11,6 @@ use App\Http\Controllers\usersController;
 use App\Http\Controllers\utilitiesPaymentController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest:web')->group(function () {
-    #GENERAL auth routes, will change to a differnt login flow later
-    Route::get('/login', [authController::class, 'login'])->name('login');
-    Route::post('/login', [authController::class, 'loginAction'])->name('loginAction');
-    Route::get('/registration', [authController::class, 'registration'])->name('registration');
-    Route::post('/registration-action', [authController::class, 'registrationAction'])->name('registrationAction');
-    Route::get('/forget_password', [authController::class, 'forget_password'])->name('forget_password');
-    Route::post('/forgetPassword', [authController::class, 'forgetPasswordMail'])->name('forgetPassword');
-    Route::get('/password/reset/{token}', [authController::class, 'showResetForm'])->name('password.reset');
-    Route::post('/update-password', [authController::class, 'updatePassword'])->name('updatePassword');
-    Route::get('/logout', [authController::class, 'logout'])->name('logout');
-});
-
 Route::domain('{slug}.theeprojects.test')->middleware(['identify.merchant'])->group(function () {
     Route::get('/', function () {
         return view('template-layout.index');
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,21 @@ use App\Http\Controllers\adminController;
 use App\Http\Controllers\authController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [authController::class, 'login'])->name('login');
+Route::middleware('guest:web')->group(function () {
+    #GENERAL auth routes, will change to a differnt login flow later
+    Route::get('/login', function () {
+        return view('welcome');
+    })->name('login');
+    Route::post('/login', [authController::class, 'loginAction'])->name('loginAction');
+    Route::get('/registration', [authController::class, 'registration'])->name('registration');
+    Route::post('/registration-action', [authController::class, 'registrationAction'])->name('registrationAction');
+    Route::get('/forget_password', [authController::class, 'forget_password'])->name('forget_password');
+    Route::post('/forgetPassword', [authController::class, 'forgetPasswordMail'])->name('forgetPassword');
+    Route::get('/password/reset/{token}', [authController::class, 'showResetForm'])->name('password.reset');
+    Route::post('/update-password', [authController::class, 'updatePassword'])->name('updatePassword');
+    Route::get('/logout', [authController::class, 'logout'])->name('logout');
+});
+
 Route::middleware(['auth:admin'])->group(function () {
 #admin routes
     Route::get('/admin/dashboard', [adminController::class, 'dashboard'])->name('admin.dashboard');
